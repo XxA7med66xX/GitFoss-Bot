3f13f18e6e1b37c89c776de4717aaeb8e038397d
XxA7med66xX <luis9898901@gmail.com>
2023-03-19 11:27:13 +0300

    The create command was implemented with it's ability to organize github project info

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,4 @@
-import 'dart:ffi';
-
+import 'package:rog_telegram_bot/commands/Create_and_prepare.dart';
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 import 'package:teledart/model.dart';
@@ -8,6 +7,7 @@ import 'package:meta/dart2js.dart';
 import 'package:test/expect.dart';
 import 'package:rog_telegram_bot/models/URLprocess/HttpFetchURLInfo.dart';
 void main() async {
+  
   //Initializing Bot Config
   var BOT_TOKEN = '6059435916:AAF_SWPEH02zCERYkS199NpHDKN3DInHmGI';
   final username = (await Telegram(BOT_TOKEN).getMe()).username;
@@ -19,24 +19,33 @@ void main() async {
   StartHelpCommands.Start_Help_commands();
   
   //Take the url and fetch them to extract the paragraph
-  final httpfetchurl = HttpFetchURL();
-  httpfetchurl.URLfetch_InfoParse();
   
 
-  //GitHub - NVIDIA/open-gpu-kernel-modules: NVIDIA Linux open GPU kernel module source
+  //Create command, to create and prepare the post
+  final Create_and_prepare = create_and_prepare(teledart: teledart,);
+  Create_and_prepare.CreateCommand();
+  Create_and_prepare.SaveUserInput();
   
 
 
-  
-  
+
 }
 
-
-
-
-
-
   
 
 
 
+
+// update.listen((update) {
+  //   if(update.message == "/create"){
+  //     teledart.sendMessage(
+  //       update.message!.chat.id,
+  //       "Send github link",
+  //     );
+  //     String? GitLink = stdin.readLineSync();
+  //     print(GitLink);
+  //   }
+  // });
+
+
+  //GitHub - NVIDIA/open-gpu-kernel-modules: NVIDIA Linux open GPU kernel module source
--- lib/models/URLprocess/HttpFetchURLInfo.dart
+++ lib/models/URLprocess/HttpFetchURLInfo.dart
@@ -1,25 +1,55 @@
+import 'dart:io';
 import 'package:html/parser.dart' as html;
 import 'package:http/http.dart' as http;
+import 'package:rog_telegram_bot/commands/Create_and_prepare.dart';
+import 'package:teledart/teledart.dart';
 
 class HttpFetchURL {
+  HttpFetchURL({this.URL="", required this.teledart,this.TEST});
 
+  //Assigned variables
+  String URL;
+  TeleDart teledart;
+  String? TEST;
+  
   URLfetch_InfoParse() async {
-
-    var URL = "https://github.com/ahkarami/Deep-Learning-in-Production";
-
+    //Here function parsing the URL to be able to extract element's from it
     var response = await http.get(Uri.parse(URL));
-
     var Document = html.parse(response.body);
 
+    //Here this variable save the extracted title as a text 
     var title = Document.head!.querySelector('title')!.text;
+    
+    //Method to bring the name of project author
+    String keyword1 = '-';
+    String endword1 = '/';
+    int keyword_index1 = title.indexOf(keyword1);
+    int endword_index1 = title.indexOf(endword1);
+    
+    //Method to bring the name of project
+    String keyword2 = '/';
+    String endword2 = ':';
+    final int keyword_index2 = title.indexOf(keyword2) + 1;
+    final int endword_index2 = title.indexOf(endword2);
 
-    String keyword = '/';
-    String Endword = ':';
-    final int keywordIndex = title.indexOf(keyword) + 1;
-    final int endkeywordIndex = title.indexOf(Endword);
-    String substring = title.substring(keywordIndex, endkeywordIndex);
-    print(substring);
+    //Method to extract the description of project
+    String keyword3 = ':';
+    String endword3 = '.';
+    int keyword_index3 = title.indexOf(keyword3);
+    int endword_index3 = title.length;
+    
+    //The result of the method's above
+    String author_name = title.substring(keyword_index1 + 2, endword_index1);
+    String project_Title = title.substring(keyword_index2, endword_index2);
+    String project_description = title.substring(keyword_index3 + 1,endword_index3);
 
+    Map<String, dynamic> StringX = {
+      'Ptitle': project_Title,
+      'Aname': author_name,
+      'Pdescription' : project_description
+    };
+
+    print(project_description); //just for testing the outputs
+    return StringX; 
   }
-
 }
