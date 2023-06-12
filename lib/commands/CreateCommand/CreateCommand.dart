43015f8faf783e5253a74da9a30903a402221d9f
XxA7med66xX <abczxx20132014@gmail.com>
2023-06-12 12:35:37 +0300

    Added condition check to only process URLs when user sends /Create command

--- lib/commands/CreateCommand/CreateCommand.dart
+++ lib/commands/CreateCommand/CreateCommand.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:GitFossBOT/commands/CreateCommand/PostCreate/githubCreate.dart';
+import 'package:teledart/model.dart';
 import 'package:teledart/teledart.dart';
 
 class createCommand {
@@ -6,18 +8,20 @@ class createCommand {
     required this.teledart,
     this.GitLink = "",
   });
-
+  
   final TeleDart teledart;
   String GitLink; //The github link that would received from user.
   String Project_title = '';
   String Author_name = '';
   String Project_description = '';
   static const List<String> defaultURLs = ['github.com'];
-
+  bool isCreating = false;
+  
   //This function used to set /create command on the bot.
   CreateCommand() {
     teledart.onCommand('create').listen(
       (message) async {
+        isCreating = true;
         await teledart.sendMessage(
           message.chat.id,
           'الرجاء إرسال رابط لمشروع على Github أو Gitlab',
@@ -29,7 +33,8 @@ class createCommand {
   SaveUserInput() async {
     teledart.onMessage(entityType: 'url').listen(
       (message) async {
-
+        if (!isCreating) return;
+        
         GitLink = message.text!;
         
         switch (defaultURLs.indexWhere((defaultURL) => GitLink.contains(defaultURL))) {
