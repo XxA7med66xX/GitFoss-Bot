63a7e3608c3b526d24ab2fa515d74cbeff57ae45
XxA7med66xX <abczxx20132014@gmail.com>
2023-06-18 15:33:27 +0300

    Set one variable to retrieve all information about a GitHub project

--- lib/models/PrepareInfo/githubPrepare/githubPerpare.dart
+++ lib/models/PrepareInfo/githubPrepare/githubPerpare.dart
@@ -1,7 +1,5 @@
-import 'package:GitFossBOT/models/PrepareInfo/githubPrepare/utils/GithubSubStrings.dart';
-import 'package:GitFossBOT/models/PrepareInfo/githubPrepare/utils/getProjectReleases.dart';
+import 'package:GitFossBOT/models/PrepareInfo/githubPrepare/utils/FlatSnap/getAllProjectInfo.dart';
 import 'package:GitFossBOT/models/PrepareInfo/githubPrepare/utils/getReleaseVersion.dart';
-import 'package:GitFossBOT/models/PrepareInfo/githubPrepare/utils/getTitle.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as html;
 import 'package:teledart/model.dart';
@@ -38,23 +36,9 @@ class githubInfoPerpare {
     final response3 = await http.get(Uri.parse(releases_tag_url));
     final document3 = html.parse(response3.body);
     
-    final String title = getTitle().Title(document);
-    final String author_name = Github_subStrings().AuthorName(title);
-    final String project_Title = Github_subStrings().ProjectName(title);
-    final String project_description = Github_subStrings().ProjectDescription(title);
-    final List<dynamic> project_Releases = getProjectReleases().ProjectReleases(document, document3);
+    final Map<String,dynamic> AllProjectInfo = getAllProjectInfo().AllprojectInfo(document, document3, releaseVersion);
 
-    final Map<String, dynamic> projectInfo = {
-      'Ptitle': project_Title,
-      'Aname': author_name,
-      'Pdescription': project_description,
-      'ProjectReleases': project_Releases,
-      'Rversion' : releaseVersion,
-    };
-
-    print(project_Releases);
-    print(releaseVersion);
-    return projectInfo;
+    return AllProjectInfo;
     
     } else {
       teleDart.sendMessage(
---
+++ lib/models/PrepareInfo/githubPrepare/utils/FlatSnap/getAllProjectInfo.dart
@@ -0,0 +1,26 @@
+import 'package:GitFossBOT/models/PrepareInfo/githubPrepare/utils/GithubSubStrings.dart';
+import 'package:GitFossBOT/models/PrepareInfo/githubPrepare/utils/getProjectReleases.dart';
+import 'package:GitFossBOT/models/PrepareInfo/githubPrepare/utils/getTitle.dart';
+import 'package:html/dom.dart';
+
+class getAllProjectInfo {
+
+  Map<String,dynamic> AllprojectInfo(Document document,document3, String? releaseVersion) {
+
+    final String title = getTitle().Title(document);
+    final String author_name = Github_subStrings().AuthorName(title);
+    final String project_Title = Github_subStrings().ProjectName(title);
+    final String project_description = Github_subStrings().ProjectDescription(title);
+    final List<dynamic> project_Releases = getProjectReleases().ProjectReleases(document, document3);
+
+    final Map<String, dynamic> projectInfo = {
+      'Ptitle': project_Title,
+      'Aname': author_name,
+      'Pdescription': project_description,
+      'ProjectReleases': project_Releases,
+      'Rversion' : releaseVersion,
+    };
+
+    return projectInfo;
+  }
+}
