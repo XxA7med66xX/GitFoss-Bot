8e8ca20f13bb59847e12b0ae6eeb486808b007b0
XxA7med66xX <abczxx20132014@gmail.com>
2023-06-19 18:35:18 +0300

    Moved platform auto-sort logic to 'utils' folder in separate file

--- lib/commands/CreateCommand/PostCreate/githubCreate/githubCreate.dart
+++ lib/commands/CreateCommand/PostCreate/githubCreate/githubCreate.dart
@@ -1,4 +1,5 @@
 import 'package:GitFossBOT/commands/CreateCommand/PostCreate/githubCreate/utils/getProjectDetails.dart';
+import 'package:GitFossBOT/commands/CreateCommand/PostCreate/githubCreate/utils/platforms_AutoSort.dart';
 import 'package:teledart/model.dart';
 import 'package:teledart/teledart.dart';
 
@@ -9,99 +10,26 @@ class githubPost {
   String gitLink;
   TeleDart teleDart;
   TeleDartMessage message;
-  bool isAppImage = false;
-  bool isGnome_extention = false;
-  bool isFlatpak = false;
-  bool isSnap = false;
   
   Githubpost() async {
 
     final ProjectDetails = getProjectDetails(gitLink: gitLink, teleDart: teleDart, message: message);
     await ProjectDetails.ProjectDetails();
 
-    List<String> supportedPlatforms = [];
-    
-    List<Map<String, dynamic>> platforms = [
-        {
-          'name': 'لينكس',
-          'extensions': [
-            'AppImage',
-            'rpm',
-            'pacman',
-            'Ubuntu',
-            'deb',
-            'linux',
-            '@',
-            'gnome-shell-extension',
-            'flathub',
-            'flatpak',
-            'snap',
-            'snapcraft',
-            'bin',
-            'wine',
-          ]
-        },
-        {
-          'name': 'ويندوز',
-          'extensions': [
-            'exe',
-            'msi',
-            'cmd',
-            'bat',
-            'windows',
-            'win64',
-            'appx',
-            'msix'
-          ]
-        },
-        {
-          'name': 'ماك',
-          'extensions': ['dmg', 'macos', 'mac', 'apple', 'darwin', 'bin']
-        }
-      ];
-
-      for (Map<String, dynamic> platform in platforms) {
-        bool isSupported = false;
-        for (String extension in platform['extensions']) {
-          if (ProjectDetails.Project_Releases.any((releaseTitle) => releaseTitle.contains(extension))) {
-            isSupported = true;
-            if (extension.contains('AppImage') &&
-                extension.contains('flathub')) {
-              isAppImage = true;
-              isFlatpak = true;
-            } else if (extension.contains('AppImage')) {
-              isAppImage = true;
-            } else if (extension.contains('gnome-shell-extension')) {
-              isGnome_extention = true;
-            } else if (extension.contains('@')) {
-              isGnome_extention = true;
-            } else if (extension.contains('flathub')) {
-              isFlatpak = true;
-            } else if (extension.contains('flatpak')) {
-              isFlatpak = true;
-            } else if (extension.contains('snap')) {
-              isSnap = true;
-            } else if (extension.contains('snapcraft')) {
-              isSnap = true;
-            }
-          }
-        }
-        if (isSupported) {
-          supportedPlatforms.add(platform['name']);
-        }
-      }
+    final PlatformAutoSort = PlatformsAutoSort(ProjectReleases: ProjectDetails.Project_Releases);
+    PlatformAutoSort.AutoSort();
 
       List<String> categorys = [];
-      if (isAppImage) {
+      if (PlatformAutoSort.isAppImage) {
         categorys.add('#تطبيقات_محمولة');
       }
-      if (isGnome_extention) {
+      if (PlatformAutoSort.isGnome_extention) {
         categorys.add('#إضافات_جنوم');
       }
-      if (isFlatpak) {
+      if (PlatformAutoSort.isFlatpak) {
         categorys.add('#تطبيقات_فلاتباك');
       }
-      if (isSnap) {
+      if (PlatformAutoSort.isSnap) {
         categorys.add('#تطبيقات_سناب');
       }
       print(ProjectDetails.Project_title);
@@ -117,7 +45,7 @@ class githubPost {
 
 🔢 <b>اخر إصدار (منذ النشر)</b> : ${ProjectDetails.Release_version}
 
-💻 <b>المنصات المدعومة</b> : ${supportedPlatforms.join(' | ')}
+💻 <b>المنصات المدعومة</b> : ${PlatformAutoSort.AutoSort().join(' | ')}
 
 👤 <b>المطور</b> : ${ProjectDetails.Author_name}
 ㅤ
---
+++ lib/commands/CreateCommand/PostCreate/githubCreate/utils/platforms_AutoSort.dart
@@ -0,0 +1,86 @@
+
+class PlatformsAutoSort {
+  bool isAppImage = false;
+  bool isGnome_extention = false;
+  bool isFlatpak = false;
+  bool isSnap = false;
+  List<dynamic> ProjectReleases;
+
+  PlatformsAutoSort({required this.ProjectReleases});
+
+  AutoSort() {
+    
+    List<String> supportedPlatforms = [];
+
+    List<Map<String, dynamic>> platforms = [
+      {
+        'name': 'لينكس',
+        'extensions': [
+          'AppImage',
+          'rpm',
+          'pacman',
+          'Ubuntu',
+          'deb',
+          'linux',
+          '@',
+          'gnome-shell-extension',
+          'flathub',
+          'flatpak',
+          'snap',
+          'snapcraft',
+          'bin',
+          'wine',
+        ]
+      },
+      {
+        'name': 'ويندوز',
+        'extensions': [
+          'exe',
+          'msi',
+          'cmd',
+          'bat',
+          'windows',
+          'win64',
+          'appx',
+          'msix'
+        ]
+      },
+      {
+        'name': 'ماك',
+        'extensions': ['dmg', 'macos', 'mac', 'apple', 'darwin', 'bin']
+      }
+    ];
+
+    for (Map<String, dynamic> platform in platforms) {
+      bool isSupported = false;
+      for (String extension in platform['extensions']) {
+        if (ProjectReleases.any(
+            (releaseTitle) => releaseTitle.contains(extension))) {
+          isSupported = true;
+          if (extension.contains('AppImage') && extension.contains('flathub')) {
+            isAppImage = true;
+            isFlatpak = true;
+          } else if (extension.contains('AppImage')) {
+            isAppImage = true;
+          } else if (extension.contains('gnome-shell-extension')) {
+            isGnome_extention = true;
+          } else if (extension.contains('@')) {
+            isGnome_extention = true;
+          } else if (extension.contains('flathub')) {
+            isFlatpak = true;
+          } else if (extension.contains('flatpak')) {
+            isFlatpak = true;
+          } else if (extension.contains('snap')) {
+            isSnap = true;
+          } else if (extension.contains('snapcraft')) {
+            isSnap = true;
+          }
+        }
+      }
+      if (isSupported) {
+        supportedPlatforms.add(platform['name']);
+      }
+    }
+    return supportedPlatforms;
+  }
+}
