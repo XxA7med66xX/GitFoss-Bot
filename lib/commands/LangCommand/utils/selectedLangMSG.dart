521213de65204c46a2daa673002e9e876e2d6713
XxA7med66xX <abczxx20132014@gmail.com>
2023-07-29 08:39:01 +0300

    Handle the error in the LangCommand
    The error occurred when the user selected the default language and then selected the same language again, which caused an HTTP 400 error, This commit will address this issue.

--- lib/commands/LangCommand/utils/selectedLangMSG.dart
+++ lib/commands/LangCommand/utils/selectedLangMSG.dart
@@ -1,14 +1,27 @@
 import 'package:teledart/model.dart';
 import 'package:teledart/teledart.dart';
+import 'package:teledart/util.dart';
 
 class SelectedLangMSG {
 
   Future<Message> MSG(TeleDart teledart, Message ChooseMSG, InlineKeyboardMarkup IKM, String text) async {
 
-    return teledart.editMessageText(
-      text,
-      chatId: ChooseMSG.chat.id,
-      messageId: ChooseMSG.messageId,
-    );
+    try {
+
+      return await teledart.editMessageText(
+        text,
+        chatId: ChooseMSG.chat.id,
+        messageId: ChooseMSG.messageId,
+      );
+
+    } on HttpClientException catch (e) {
+
+      if (e.code == 400) {
+        return ChooseMSG;
+      } else {
+        rethrow;
+      }
+      
+    }
   }
 }
